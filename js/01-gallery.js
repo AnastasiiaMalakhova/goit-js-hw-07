ec814f258f91b1b39414f29302658f96ee03b287
Anastasiia Malakhova <matviichuknastya@gmail.com>
2023-01-18 21:03:51 +0200

    render

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
+const galleryEl = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>
+        `
+  )
+  .join("");
+gallery.insertAdjacentHTML("afterbegin", galleryEl);
+
+console.log(gallery);
+console.log(galleryEl);
 console.log(galleryItems);
