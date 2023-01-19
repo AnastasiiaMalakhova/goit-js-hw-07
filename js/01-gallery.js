f489629e1dd6f5a9ccfea89689e3b5c1dfab10ed
Anastasiia Malakhova <matviichuknastya@gmail.com>
2023-01-19 06:15:48 +0200

    add task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,25 +1,47 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 const gallery = document.querySelector(".gallery");
+const galleryImg = createGalleryImg(galleryItems);
 
-const galleryEl = galleryItems
-  .map(
-    ({ preview, original, description }) =>
-      `<div class="gallery__item">
+gallery.addEventListener("click", returnUrlBigImg);
+
+function createGalleryImg(items) {
+  return items
+    .map(
+      ({ preview, original, description }) =>
+        `<div class="gallery__item">
       <a class="gallery__link" href="${original}">
         <img
           class="gallery__image"
           src="${preview}"
           data-source="${original}"
           alt="${description}"
+          
         />
       </a>
     </div>
         `
-  )
-  .join("");
-gallery.insertAdjacentHTML("afterbegin", galleryEl);
+    )
+    .join("");
+}
+// gallery.insertAdjacentHTML("afterbegin", galleryImg);
+gallery.innerHTML = galleryImg;
 
-console.log(gallery);
-console.log(galleryEl);
-console.log(galleryItems);
+function returnUrlBigImg(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const bigImg = basicLightbox.create(`
+    <img src="${e.target.dataset.source}" width="800" height="600">
+  `);
+  bigImg.show();
+
+  gallery.addEventListener("keydown", ({ key }) => {
+    if (key === "Escape") {
+      bigImg.close();
+    }
+  });
+}
